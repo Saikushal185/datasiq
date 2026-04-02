1184a96c191ecae2fd1c9bda7ba4be6728e60e1e
Saikushal185 <saikushal185@gmail.com>
2026-04-02 13:12:34 +0530

    chore(frontend): update next.config.ts

---
+++ frontend/next.config.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname, "..")
+};
+
+export default withSentryConfig(nextConfig, {
+  silent: !process.env.CI
+});
